ceabe5cb67b9b761953a31ba0394f40be9064234
choijiuen <choijiuen@gmail.com>
2019-07-22 18:04:48 +0900

    dd

---
+++ struts06/src/main/java/com/bit/struts/action/DetailAction.java
@@ -0,0 +1,32 @@
+package com.bit.struts.action;
+
+import com.bit.struts.model.IbatisDao;
+import com.bit.struts.model.Struts06Dao;
+import com.bit.struts.model.entity.Struts06Vo;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class DetailAction implements Action, ModelDriven<Struts06Vo>{
+
+	private Struts06Vo bean=new Struts06Vo();
+	
+	public Struts06Vo getBean() {
+		return bean;
+	}
+	
+	public void setBean(Struts06Vo bean) {
+		this.bean = bean;
+	}
+	
+	@Override
+	public String execute() throws Exception {
+		IbatisDao dao=new IbatisDao();
+		bean= dao.listOne(bean.getNum());	//bean에 다시 대입해주기 
+		return this.SUCCESS;
+	}
+
+		public Struts06Vo getModel() {
+			return bean;
+
+		}
+}
